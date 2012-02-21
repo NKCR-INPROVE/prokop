4dca133fc71e1daa7f05258d42e0bb298cc5e083
alberto.en.vlasim <alberto.en.vlasim@gmail.com>
2012-02-21 09:05:43 +0000

    Issue 269 Fix issue 269

--- src/cz/incad/prokop/server/fast/IndexTypes.java
+++ src/cz/incad/prokop/server/fast/IndexTypes.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.incad.prokop.server.datasources.oai;
+package cz.incad.prokop.server.fast;
 
 /**
  *
