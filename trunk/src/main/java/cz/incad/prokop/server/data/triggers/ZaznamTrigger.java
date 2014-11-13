3a4195daf06bfb7d3b2c93959147246986a5ad72
vlahoda@gmail.com <vlahoda@gmail.com@01f904bf-5220-b16d-3e03-f4402af637df>
2014-11-13 12:37:08 +0000

    persiterTriggers have as parameter ContainerNode instead of Record

--- trunk/src/main/java/cz/incad/prokop/server/data/triggers/ZaznamTrigger.java
+++ trunk/src/main/java/cz/incad/prokop/server/data/triggers/ZaznamTrigger.java
@@ -5,6 +5,7 @@
 package cz.incad.prokop.server.data.triggers;
 
 import cz.incad.prokop.server.Structure;
+import org.aplikator.client.shared.data.ContainerNode;
 import org.aplikator.client.shared.data.Record;
 import org.aplikator.client.shared.descriptor.PropertyDTO;
 import org.aplikator.server.Context;
@@ -19,23 +20,23 @@ import javax.servlet.http.HttpServletRequest;
 public class ZaznamTrigger extends PersisterTriggers.Default {
 
     @Override
-    public void onCreate(Record record, Context ctx) {
+    public void onCreate(ContainerNode node, Context ctx) {
         System.out.println("Before create trigger");
         HttpServletRequest request = ctx.getHttpServletRequest();
         String remoteUser = request.getRemoteUser();
         PropertyDTO uzivatel = Structure.zaznam.uzivatel.clientClone(ctx);
         if (remoteUser != null) {
-            uzivatel.setValue(record, remoteUser);
+            uzivatel.setValue(node.getEdited(), remoteUser);
         } else {
-            uzivatel.setValue(record, " ---- ");
+            uzivatel.setValue(node.getEdited(), " ---- ");
         }
     }
 
     @Override
     public void onLoad(Record record, Context ctx) {
-        record.setPreview("<b>"+record.getValue(Structure.zaznam.hlavniNazev.getId())
-                +"</b> ("+record.getValue(Structure.zaznam.typDokumentu.getId())+")<br>"
-                +record.getValue(Structure.zaznam.urlZdroje.getId()));
+        record.setPreview("<b>"+ record.getValue(Structure.zaznam.hlavniNazev.getId())
+                +"</b> ("+ record.getValue(Structure.zaznam.typDokumentu.getId())+")<br>"
+                + record.getValue(Structure.zaznam.urlZdroje.getId()));
     }
 
 }
